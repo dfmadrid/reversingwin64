dc4151d5f2eed54f47b7a2cddc5c7ca4361cbd38
dfmadrid <dfernandez@sidertia.com>
2016-02-28 19:44:55 +0100

    Example of protected and non-protected functions

--- ReversingWin64/ReversingWin64.cpp
+++ ReversingWin64/ReversingWin64.cpp
@@ -3,9 +3,37 @@
 
 #include "stdafx.h"
 
+int multibyteToAscii(_TCHAR* mbStr, char* asciiStr)
+{
+	int i = 0;
+
+	for (i; i < _tcslen(mbStr); i++){
+		if (sizeof(_TCHAR) != sizeof(wchar_t)){
+			return 1;
+		}
+		asciiStr[i] = (char) mbStr[i];
+	}
+
+	return i;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	_TCHAR inStr[60];
+	size_t charsRead = 0;
+	char outStr[40];
+
+	memset(inStr, 0, sizeof(inStr));
+	memset(outStr, 0, sizeof(outStr));
+
+	printf("Please enter a string");
+	_cgetws_s(inStr, sizeof(inStr), &charsRead);
+
+	if (charsRead > 0){
+		multibyteToAscii(inStr, outStr);
+		printf("The ascii string received as input is %s \n", outStr);
+	}
+
 	return 0;
 }
 
--- ReversingWin64/stdafx.h
+++ ReversingWin64/stdafx.h
@@ -9,7 +9,7 @@
 
 #include <stdio.h>
 #include <tchar.h>
-
+#include <memory.h>
 
 
 // TODO: reference additional headers your program requires here
