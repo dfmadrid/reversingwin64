e4be359abf4270cb1e7de45cdc6f6220469a8333
dfmadrid <dfernandez@sidertia.com>
2016-03-20 01:52:28 +0100

    Guard Stack example with ANI vulnerability code

--- ReversingWin64/ReversingWin64.cpp
+++ ReversingWin64/ReversingWin64.cpp
@@ -3,36 +3,83 @@
 
 #include "stdafx.h"
 
-int multibyteToAscii(_TCHAR* mbStr, char* asciiStr)
-{
-	int i = 0;
+#pragma pack(1)
 
-	for (i; i < _tcslen(mbStr); i++){
-		if (sizeof(_TCHAR) != sizeof(wchar_t)){
-			return 1;
-		}
-		asciiStr[i] = (char) mbStr[i];
+typedef struct _ANICHUNK{
+	DWORD tag;
+	DWORD size;
+}ANICHUNK, *PANICHUNK;
+
+typedef struct _ANIHEADER{
+	DWORD cbSizeOf;
+	DWORD cFrames;
+	DWORD64 cSteps;
+	DWORD cx, cy; 
+	DWORD cBitCount, cPlanes;
+	DWORD JifRate; 
+	DWORD flags;
+}ANIHEADER, *PANIHEADER;
+
+void ReadTag(LPVOID hFile, PANICHUNK chunk){
+
+	memcpy(&chunk->tag, hFile, sizeof(DWORD));
+
+	if (chunk->tag != NULL)
+		memcpy(&chunk->size, ((BYTE*) hFile) + sizeof(DWORD), sizeof(DWORD));
+
+}
+
+void ReadChunk(LPVOID hFile, PANICHUNK chunk, PANIHEADER header){
+
+	memcpy(header, ((BYTE *) hFile) + (2 * sizeof(DWORD)), chunk->size);
+
+}
+
+int loadAniIcon(LPVOID hFile){
+
+	ANIHEADER header;
+	ANICHUNK chunk;
+
+	ReadTag(hFile, &chunk);
+	if (chunk.tag == 'hina')
+		ReadChunk(hFile, &chunk, &header);
+
+	return 0;
+
+}
+
+void loadAniIconFromFileMap(){
+
+	HANDLE hFile = 0;
+	HANDLE hFileMap = 0;
+	LPVOID structMappedFile = 0;
+
+	hFile = CreateFile(L"icon.ico", GENERIC_READ, FILE_SHARE_READ, NULL,
+		OPEN_EXISTING, 0, NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("Unable to find icon file"));
+		return;
 	}
 
-	return i;
+	hFileMap = CreateFileMapping(hFile,
+		NULL,
+		PAGE_READONLY,
+		0,
+		1,
+		NULL);
+
+	structMappedFile = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 0);
+
+	loadAniIcon(structMappedFile);
+
+	CloseHandle(hFile);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	_TCHAR inStr[60];
-	size_t charsRead = 0;
-	char outStr[40];
-
-	memset(inStr, 0, sizeof(inStr));
-	memset(outStr, 0, sizeof(outStr));
-
-	printf("Please enter a string");
-	_cgetws_s(inStr, sizeof(inStr), &charsRead);
-
-	if (charsRead > 0){
-		multibyteToAscii(inStr, outStr);
-		printf("The ascii string received as input is %s \n", outStr);
-	}
+	loadAniIconFromFileMap();
 
 	return 0;
 }
--- ReversingWin64/stdafx.h
+++ ReversingWin64/stdafx.h
@@ -13,3 +13,5 @@
 
 
 // TODO: reference additional headers your program requires here
+
+#include <Windows.h>
